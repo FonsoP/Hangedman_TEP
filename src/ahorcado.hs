f3aa3ae15354e9b0827a62c131d1d9851b7ddb73
Fer2703 <fabiferchu@gmail.com>
2025-05-29 23:52:03 -0400

    feat: Menu de opciones
    Menu de opciones con las funcionalidades de Jugar Partida, Visualizar estadisticas y Salir del Juego

--- src/ahorcado.hs
+++ src/ahorcado.hs
@@ -2,13 +2,23 @@ import System.IO
 import Data.Char
 import Data.List
 import System.Directory
+import System.Console.ANSI
 
 escribir :: String -> String
 escribir(palabra) = take (length palabra) (repeat '_')
 
+mostrarMenu :: IO ()
+mostrarMenu = do
+    clearScreen
+    putStrLn "\n----- EL AHORCADO: -----"
+    putStrLn "1. Jugar Partida"
+    putStrLn "2. Visualizar Estadísticas"
+    putStrLn "3. Salir del Programa"
+    putStrLn "Selecciona una opcion (1-3):"
 
-main = do
-    
+jugarPartida :: IO ()
+jugarPartida = do
+    clearScreen
     -- lectura del archivo
     handle <- openFile "src/palabras.txt" ReadMode
 
@@ -21,12 +31,45 @@ main = do
     -- mostrar en pantalla la palabra en minusculas
     putStrLn "Dí una letra: "
     
+    -- Lee la letra ingresada por el usuario
     letra <- getLine
 
+    -- Muestra todas las palabras del archivo
     putStr $ unlines palabrasList 
 
+    -- Muestra la primera palabra del archivo oculta con guiones bajos
     putStr $ escribir(palabrasList !! 0) ++ "\n"
 
+    -- Muestra la letra que ingresó el usuario
     putStr letra
+
+visualizarEstadisticas :: IO ()
+visualizarEstadisticas = do
+    clearScreen
+    putStrLn "--- ESTADÍSTICAS ---"
+
+-- MAIN --
+main = do
+    let loop = do
+        mostrarMenu
+        opcion <- getLine
+        case opcion of
+            "1" -> do
+                jugarPartida
+                putStrLn "\nPresione Enter para continuar..."
+                _ <- getLine
+                loop
+            "2" -> do
+                visualizarEstadisticas
+                putStrLn "\nPresione Enter para continuar..."
+                _ <- getLine
+                loop
+            "3" -> putStrLn "Gracias por jugar!"
+            _ -> do
+                putStrLn "Opción no válida. Por favor seleccione un número del 1 al 3."
+                putStrLn "Presione Enter para continuar..."
+                _ <- getLine
+                loop
+    loop
 
     
