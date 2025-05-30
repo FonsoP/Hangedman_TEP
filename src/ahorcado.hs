e63d21583bf25831944dcaf8a2e2ff32a306fee6
Fer2703 <fabiferchu@gmail.com>
2025-05-30 00:31:20 -0400

    fix: Errores de identado y compatibilidad
    Solucionado

--- src/ahorcado.hs
+++ src/ahorcado.hs
@@ -2,30 +2,26 @@ import System.IO
 import Data.Char
 import Data.List
 import System.Directory
-import System.Console.ANSI
+import System.Process
+import System.Info
 
--- crea la palabra cubierta con guiones bajos
-cubrir :: String -> String
-cubrir palabra = take (length palabra) (repeat '_')
+escribir :: String -> String
+escribir palabra = replicate (length palabra) '_'
 
-descubrir :: (Int, String, Char) -> String
-descubrir (indice, cubierta, letra) =  take indice cubierta ++ [letra] ++ drop (indice + 1) cubierta;
-        
-
-buscar:: (String, Char) -> Maybe Int
-buscar(palabra, letra) = elemIndex letra palabra
-    
-
-procesarLetra :: String -> String -> Char -> IO ()
-procesarLetra palabra cubierta letra = 
-    case buscar (palabra, letra) of
-        Just idx -> putStrLn $ descubrir (idx, cubierta, letra)
-        Nothing  -> putStrLn "La letra no está en la palabra."
+limpiarPantalla :: IO ()
+limpiarPantalla = do
+    if os == "mingw32" || os == "win32"
+        then do
+            _ <- system "cls"
+            return ()
+        else do
+            _ <- system "clear"
+            return ()
 
 mostrarMenu :: IO ()
 mostrarMenu = do
-    clearScreen
-    putStrLn "\n----- EL AHORCADO: -----"
+    limpiarPantalla
+    putStrLn "----- EL AHORCADO: -----"
     putStrLn "1. Jugar Partida"
     putStrLn "2. Visualizar Estadísticas"
     putStrLn "3. Salir del Programa"
@@ -33,7 +29,7 @@ mostrarMenu = do
 
 jugarPartida :: IO ()
 jugarPartida = do
-    clearScreen
+    limpiarPantalla
     -- lectura del archivo
     handle <- openFile "src/palabras.txt" ReadMode
 
@@ -41,7 +37,7 @@ jugarPartida = do
     contents <- hGetContents handle
     
     -- crea un arreglo con las lineas del archivo
-    let palabrasList = lines  contents
+    let palabrasList = lines contents
     
     -- mostrar en pantalla la palabra en minusculas
     putStrLn "Dí una letra: "
@@ -53,7 +49,7 @@ jugarPartida = do
     putStr $ unlines palabrasList 
 
     -- Muestra la primera palabra del archivo oculta con guiones bajos
-    putStr $ escribir(palabrasList !! 0) ++ "\n"
+    putStr $ escribir(head palabrasList)
 
     -- Muestra la letra que ingresó el usuario
     putStr letra
@@ -66,82 +62,47 @@ leerEstadisticas = do
             contenido <- readFile "src/estadisticas.txt"
             let lineas = lines contenido
             if length lineas >= 3
-                then return (read (lineas !! 0), read (lineas !! 1), read (lineas !! 2))
+                then return (read (head lineas), read (lineas !! 1), read (lineas !! 2))
                 else return (0, 0, 0)
         else return (0, 0, 0)
 
 visualizarEstadisticas :: IO ()
 visualizarEstadisticas = do
-    clearScreen
-    putStrLn "=== ESTADÍSTICAS DEL JUEGO ==="
+    limpiarPantalla
+    putStrLn "--- ESTADÍSTICAS DEL JUEGO ---"
     (ganadas, perdidas, abandonadas) <- leerEstadisticas
     putStrLn $ "Partidas ganadas: " ++ show ganadas
     putStrLn $ "Partidas perdidas: " ++ show perdidas
     putStrLn $ "Partidas abandonadas: " ++ show abandonadas
     putStrLn $ "Total de partidas: " ++ show (ganadas + perdidas + abandonadas)
 
--- MAIN --
+main :: IO ()
 main = do
-
-  -- lectura del archivo
-    handle <- openFile "src/palabras.txt" ReadMode
-
-    -- convertir el contenido a una lista de palabras
-    contents <- hGetContents handle
-    
-    -- crea un arreglo con las lineas del archivo
-    let palabrasList = map (map toLower) (lines contents)
-    
-    -- pone el arreglo separado por \n
-    putStr $ unlines palabrasList 
-
-    let palabraElegida =  palabrasList !! 3
-
-    let cubierta =  cubrir palabraElegida
-
-    let intentos = length palabraElegida
-
-    jugar palabraElegida cubierta intentos
-
-
-
-
-
-
-
-
-    putStrLn "Palabra cubierta: "
-    putStrLn cubierta
-
-    putStrLn "Dí una letra: "
-    --leer de pantalla
-    letra <- getLine
-
-    procesarLetra palabraElegida cubierta (head letra)
-
-
     let loop = do
-        mostrarMenu
-        opcion <- getLine
-        case opcion of
-            "1" -> do
-                jugarPartida
-                putStrLn "\nPresione Enter para continuar..."
-                _ <- getLine
-                loop
-            "2" -> do
-                visualizarEstadisticas
-                putStrLn "\nPresione Enter para continuar..."
-                _ <- getLine
-                loop
-            "3" -> putStrLn "Gracias por jugar!"
-            _ -> do
-                putStrLn "\n❌ Opción no válida. Por favor seleccione un número del 1 al 3."
-                putStrLn "Presione Enter para continuar..."
-                _ <- getLine
-                loop
+            mostrarMenu
+            opcion <- getLine
+            case opcion of
+                "1" -> do
+                    jugarPartida
+                    putStrLn "\nPresione Enter para continuar..."
+                    _ <- getLine
+                    loop
+                "2" -> do
+                    visualizarEstadisticas
+                    putStrLn "\nPresione Enter para continuar..."
+                    _ <- getLine
+                    loop
+                "3" -> do
+                    limpiarPantalla
+                    putStrLn "¡Gracias por jugar!"
+                    putStrLn "Presione Enter para salir..."
+                    _ <- getLine
+                    return ()
+                _ -> do
+                    putStrLn "\n❌ Opción no válida. Por favor seleccione un número del 1 al 3."
+                    putStrLn "Presione Enter para continuar..."
+                    _ <- getLine
+                    loop
     loop
 
-
-
-  
+    
